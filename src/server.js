f0e3ed1af94cf5bf202dd893fb5a5fb7f8934b63
joaoGFS <joaoskatis1000@gmail.com>
2021-01-03 19:26:02 -0400

    CORS

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const fs = require('fs')
+const cors = require("cors")
 
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
